92425829531687d125f9640cf64ae6d96a56479f
Leonardo Luz Fachel <leonardoluz.f02@gmail.com>
2026-01-09 19:48:02 -0300

    feat: add env file path, mongoose config and users, products and stock modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import validationConfig from './config/validation.config';
+import { UsersModule } from './modules/users/users.module';
+import { ProductsModule } from './modules/products/products.module';
+import { StockMovementModule } from './modules/stock-movement/stock-movement.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [validationConfig],
+      envFilePath: [
+        `.env.${process.env.NODE_ENV}`,
+        `.env.${process.env.NODE_ENV}.local`,
+        '.env.local',
+      ]
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    ProductsModule,
+    StockMovementModule,
+  ]
 })
-export class AppModule {}
+export class AppModule { }
