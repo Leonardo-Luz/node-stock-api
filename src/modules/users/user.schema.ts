a1bfe2703d5e713654bfbdd4f1daf0f24741ef55
Leonardo Luz Fachel <leonardoluz.f02@gmail.com>
2026-01-09 19:38:34 -0300

    feat: add user schema

---
+++ src/modules/users/user.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+import { UserRole } from '@enums/user-role.enum';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema({ timestamps: true, collection: 'users' })
+export class User {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true, select: false })
+  password: string;
+
+  @Prop({ type: String, enum: UserRole, default: UserRole.VIEWER })
+  role: UserRole;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
