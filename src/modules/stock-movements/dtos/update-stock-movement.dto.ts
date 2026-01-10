63f139a8bc7cc12dc286d333af20b7cdc27c3543
Leonardo Luz Fachel <leonardoluz.f02@gmail.com>
2026-01-10 16:08:41 -0300

    chore: rename stock movement to plural

---
+++ src/modules/stock-movements/dtos/update-stock-movement.dto.ts
@@ -0,0 +1,34 @@
+import { StockMovementReason } from '@enums/stock-movement-reason.enum';
+import { StockMovementType } from '@enums/stock-movement-type.enum';
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsEnum, IsMongoId, IsNumber, IsOptional, Min } from 'class-validator';
+
+export class UpdateStockMovementDto {
+  @IsOptional()
+  @IsMongoId()
+  @ApiProperty({ required: false, example: '69616c3558a5f808452e616c' })
+  productId?: string;
+
+  @IsOptional()
+  @IsNumber()
+  @Type(() => Number)
+  @Min(0)
+  @ApiProperty({ required: false, example: 12 })
+  quantity?: number;
+
+  @IsOptional()
+  @IsEnum(StockMovementType)
+  @ApiProperty({ required: false, type: String, enum: StockMovementType, example: StockMovementType.IN })
+  type?: StockMovementType;
+
+  @IsOptional()
+  @IsEnum(StockMovementReason)
+  @ApiProperty({ required: false, type: String, enum: StockMovementReason, example: StockMovementReason.PURCHASE })
+  reason?: StockMovementReason;
+
+  @IsOptional()
+  @IsMongoId()
+  @ApiProperty({ required: false, example: '69616b6b09612500b6b11faa' })
+  createdBy?: string;
+}
---
+++ src/modules/stock-movements/stock-movements.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { StockMovementsService } from './stock-movements.service';
+import { StockMovementsController } from './stock-movements.controller';
+import { StockMovement, StockMovementSchema } from './stock-movement.schema';
+import { Product, ProductSchema } from '../products/product.schema';
+import { User, UserSchema } from '../users/user.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: StockMovement.name, schema: StockMovementSchema },
+      { name: Product.name, schema: ProductSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
+  ],
+  providers: [StockMovementsService],
+  controllers: [StockMovementsController],
+  exports: [StockMovementsService],
+})
+export class StockMovementsModule { }
