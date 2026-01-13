6e5c8f60415353959ae398d5eaa08ec5cbcfe56e
Leonardo Luz Fachel <leonardoluz.f02@gmail.com>
2026-01-13 14:54:24 -0300

    feat: add roles guard
    Added roles guard for role based endpoint protection using roles decorator

---
+++ src/modules/auth/guards/roles.guard.ts
@@ -0,0 +1,33 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { UserRole } from '@enums/user-role.enum';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) { }
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles =
+      this.reflector.getAllAndOverride<UserRole[]>(
+        ROLES_KEY,
+        [context.getHandler(), context.getClass()],
+      );
+
+    if (!requiredRoles || requiredRoles.length === 0) {
+      return true;
+    }
+
+    const req = context.switchToHttp().getRequest();
+
+    if (!req.user || !req.user.role) {
+      return false;
+    }
+
+    return requiredRoles.includes(req.user.role);
+  }
+}
