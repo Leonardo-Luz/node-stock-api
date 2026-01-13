a3c2ab48d9dbccaee08e6da680de7e3051a49eb5
Leonardo Luz Fachel <leonardoluz.f02@gmail.com>
2026-01-13 14:32:11 -0300

    feat: add jwt env validation

--- src/config/validation.config.ts
+++ src/config/validation.config.ts
@@ -6,12 +6,19 @@ export default registerAs('validation', () => ({
     NODE_ENV: Joi.string()
       .valid('development', 'production', 'test')
       .default('development'),
+    HOST: Joi.string()
+      .hostname()
+      .default('localhost'),
     PORT: Joi.number()
       .port()
       .default(3000),
     MONGO_URI: Joi.string()
       .pattern(/^mongodb(\+srv)?:\/\/.+/)
       .message('MONGO_URI must be a valid MongoDB connection string')
+      .required(),
+    ACCESS_SECRET: Joi.string()
+      .required(),
+    REFRESH_SECRET: Joi.string()
       .required(),
   }),
 }));
