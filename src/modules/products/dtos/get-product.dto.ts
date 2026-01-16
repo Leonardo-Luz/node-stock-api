c8eff01e1a82f7486e180a057323987bb3339c3b
Leonardo Luz Fachel <leonardoluz.f02@gmail.com>
2026-01-15 23:47:05 -0300

    feat: add class transformer anotations
    Added expose class transformer anotations to use plain to instance method

--- src/modules/products/dtos/get-product.dto.ts
+++ src/modules/products/dtos/get-product.dto.ts
@@ -1,22 +1,28 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { ProductStatus } from '@enums/product-status.enum';
+import { Expose } from 'class-transformer';
 
 export class GetProductDto {
+  @Expose({ name: '_id' })
   @ApiProperty({ example: '695effb8efab678ec35554fd' })
   id: string;
 
+  @Expose()
   @ApiProperty({ required: true, example: 'White Rice 5kg' })
   name: string;
 
+  @Expose()
   @ApiProperty({
     required: false,
     example: 'Premium long-grain white rice, 5kg package',
   })
   description?: string;
 
+  @Expose()
   @ApiProperty({ required: true, example: 24.99 })
   price: number;
 
+  @Expose()
   @ApiProperty({
     required: true,
     example: 120,
@@ -25,9 +31,11 @@ export class GetProductDto {
   })
   currentStock: number;
 
+  @Expose()
   @ApiProperty({ required: true, example: 'Groceries' })
   category: string;
 
+  @Expose()
   @ApiProperty({
     enum: ProductStatus,
     example: ProductStatus.ACTIVE,
@@ -35,9 +43,11 @@ export class GetProductDto {
   })
   status?: string;
 
+  @Expose()
   @ApiProperty({ type: String, format: 'date-time' })
   createdAt: Date;
 
+  @Expose()
   @ApiProperty({ type: String, format: 'date-time' })
   updatedAt: Date;
 }
