6e788a8440281732a0d5fd103cba505edf922dba
Leonardo Luz Fachel <leonardoluz.f02@gmail.com>
2026-01-17 00:15:34 -0300

    feat: add swagger anotations

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -10,7 +10,7 @@ import {
 import { AuthService } from './auth.service';
 import { SignUpDto } from './dtos/sign-up.dto';
 import { Public } from './decorators/public.decorator';
-import { ApiBody, ApiCookieAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiCookieAuth, ApiProperty, ApiResponse, ApiTags } from '@nestjs/swagger';
 import type { Request, Response } from 'express';
 import {
   clearAuthCookies,
@@ -22,14 +22,29 @@ import { SignInDto } from './dtos/sign-in.dto';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { RefreshAuthGuard } from './guards/refresh.guard';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { UserRole } from '@enums/user-role.enum';
+
+export class MeResponse {
+  @ApiProperty({ example: '695effb8efab678ec35554fd' })
+  id: string;
+
+  @ApiProperty({ example: UserRole.VIEWER })
+  role: UserRole;
+}
+
+export class SuccessResponse {
+  @ApiProperty({ example: true })
+  success: boolean;
+}
 
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
   @Public()
   @Post('sign-up')
+  @ApiResponse({ type: SuccessResponse })
   async signUp(@Body() dto: SignUpDto) {
     return this.authService.register(dto);
   }
@@ -38,6 +53,7 @@ export class AuthController {
   @Post('sign-in')
   @UseGuards(LocalAuthGuard)
   @ApiBody({ type: SignInDto })
+  @ApiResponse({ type: SuccessResponse })
   async signIn(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
     const user = req.user as GetPasswordUserDto;
 
@@ -53,6 +69,7 @@ export class AuthController {
   @UseGuards(RefreshAuthGuard)
   @Post('sign-out')
   @ApiCookieAuth('refresh-token')
+  @ApiResponse({ type: SuccessResponse })
   async signOut(
     @Req() req: Request,
     @Res({ passthrough: true }) res: Response,
@@ -69,6 +86,7 @@ export class AuthController {
   @UseGuards(RefreshAuthGuard)
   @Get('refresh')
   @ApiCookieAuth('refresh-token')
+  @ApiResponse({ type: SuccessResponse })
   async regenerateTokens(
     @Req() req: Request,
     @Res({ passthrough: true }) res: Response,
@@ -88,6 +106,7 @@ export class AuthController {
   @UseGuards(JwtAuthGuard)
   @Get('me')
   @ApiCookieAuth('access-token')
+  @ApiResponse({ type: MeResponse })
   me(@Req() req: Request) {
     return req.user;
   }
